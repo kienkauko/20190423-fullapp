b41a4bd9437b9569b75714601ae42858edd563f6
kienkauko <nguyentrungkiendhv@gmail.com>
2019-04-23 11:52:15 +0700

    dit cu may

---
+++ fullapp-master/ReceiveDensity/all_lib.hpp
@@ -0,0 +1,26 @@
+#include <time.h>
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <getopt.h>             /* getopt_long() */
+#include <fcntl.h>              /* low-level i/o */
+#include <unistd.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/ioctl.h>
+#include <stdint.h>
+#include <sys/mman.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <linux/videodev2.h>
+#include "NEON_2_SSE.hpp"
+#include <unistd.h>
+
+using namespace std; 
---
+++ fullapp-master/decodeGray/rgb2gray.hpp
@@ -0,0 +1,8 @@
+#include <time.h>
+#include <iostream>
+//#include <arm_neon.h>
+#include "NEON_2_SSE.hpp"
+
+using namespace std;
+
+void rgbtogray (uint8_t red[307200], uint8_t green[307200], uint8_t blue[307200], uint8_t *gray_p);
